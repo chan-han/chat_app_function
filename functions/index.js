176d3ddb0a99e487d7816a6aee1c7cc7d379cf9b
chan-han <hch99000@gmail.com>
2020-09-01 20:08:15 +0900

    "tfire = true tlog 생성"

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 
-exports.sendStatusMessage = functions.firestore.document('buildings/{bId}/gateways/{gId}/things/{tId}').onWrite(async (change, context) => {
+exports.sendStatusMessage = functions.firestore.document('buildings/{bId}/gateways/{gId}/things/{tId}').onUpdate(async (change, context) => {
 
     const bId = context.params.bId;
+    const previousData = change.before.data().tfire;
     const data = change.after.data().tfire;
-    const previousData = change.before.data();
-    functions.logger.log('message', data);
-    functions.logger.log('bid', bId);
     const users = admin.firestore().collection('users');
 
     let bname = await admin.firestore()
@@ -32,21 +30,33 @@ exports.sendStatusMessage = functions.firestore.document('buildings/{bId}/gatewa
     };
 
 
-    await users.get()
-        .then(snapshot => {
-            snapshot.forEach(doc => {
-                functions.logger.log('doc.id', doc.id);
-                const pushToken = doc.data().token;
-                functions.logger.log('토큰값, 보내는 메세지', pushToken, payload);
-                if (data === true)
-                admin.messaging().sendToDevice(pushToken, payload);
+    if (data === previousData)
+        return null;
+    else if (data === false)
+        return null;
+    else await users.get()
+            .then(snapshot => {
+                snapshot.forEach(doc => {
+                    functions.logger.log('doc.id', doc.id);
+                    const pushToken = doc.data().token;
+                    functions.logger.log('토큰값, 보내는 메세지', pushToken, payload);
 
+                    admin.messaging().sendToDevice(pushToken, payload);
+
+                });
+                return '모든 유저에게 보내는 메세지 입니다.';
+            })
+            .catch(err => {
+                functions.logger.log('에러', err);
             });
-            return '모든 유저에게 보내는 메세지 입니다.';
-        })
-        .catch(err => {
-            functions.logger.log('에러', err);
-        });
+
+    return change.after.ref.update(
+        {"tlog": admin.firestore.FieldValue.arrayUnion({description: "test", ITime: new Date()})}
+    );
 
 
 });
+
+/*
+if (data === true)
+    admin.messaging().sendToDevice(pushToken, payload);*/
